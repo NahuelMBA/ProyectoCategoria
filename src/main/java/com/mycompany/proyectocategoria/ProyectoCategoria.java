edf0df8987234d5075186a36ba6079b659900bed
NahuelMBA <namercado28@gmail.com>
2023-08-23 19:04:03 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/proyectocategoria/ProyectoCategoria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocategoria;
+
+/**
+ *
+ * @author nahue
+ */
+public class ProyectoCategoria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
